2951d5fd549b8e9187a8520fa4b9f6e8c7c7ef91
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2018-10-22 14:43:17 +0300

    Fix:  - context id formatting  - timestamp formatting  - additional format tests  - additional validations

--- lib/id_generator.rb
+++ lib/id_generator.rb
@@ -7,29 +7,29 @@ class IdGenerator
   end
 
   def initialize(context_id)
-    @context_id = context_id.to_i
+    raise(IdGenerator::Error, 'Invalid project id') unless context_id_valid?(context_id)
+    @context_id = value_to_hex(context_id, 2)
   end
 
   def generate
-    validate_context_id
-
     "#{time}-#{@context_id}-#{random_number}"
   end
 
   private
 
-  def validate_context_id
-    return if context_id_valid?
-
-    raise(IdGenerator::Error, 'Invalid project id')
-  end
-
-  def context_id_valid?
-    @context_id.positive?
+  def context_id_valid?(context_id)
+    return false unless context_id.is_a?(Integer)
+    return false unless context_id < 256
+    return false unless context_id > 0
+    true
   end
 
   def time
-    (Time.now.to_i - Time.new(2014).to_i).to_s(16)
+    value_to_hex(Time.now.to_i - Time.new(2014).to_i, 8)
+  end
+
+  def value_to_hex(value, size)
+    sprintf("%0#{size}x", value)
   end
 
   def random_number
--- spec/id_generator/generator_spec.rb
+++ spec/id_generator/generator_spec.rb
@@ -2,34 +2,82 @@ RSpec.describe IdGenerator do
   let(:generator) { described_class.new(context_id) }
 
   describe 'id format' do
-    let(:context_id) { 999 }
+    let(:context_id) { rand(255) }
+    let(:parts) { uniq_id.split('-') }
 
     it 'has correct size' do
       expect(uniq_id.size).to eq(34)
     end
 
+    it 'has correct format' do
+      expect(uniq_id).to match(/^[a-z0-9\-]+$/)
+    end
+
+    it 'has 2 separators' do
+      expect(parts.size).to eq(3)
+    end
+
+    it 'has correct timestamp size' do
+      expect(parts[0].size).to eq(8)
+    end
+
+    it 'has correct project id size' do
+      expect(parts[1].size).to eq(2)
+    end
+
+    it 'has correct random part size' do
+      expect(parts[2].size).to eq(22)
+    end
+
     it 'includes timestamp' do
       timestamp = Time.now.to_i - Time.new(2014).to_i
       id_timestamp = uniq_id.split('-').first.to_i(16)
-      expect(id_timestamp).to be_between(timestamp, timestamp+2)
+      expect(id_timestamp).to be_between(timestamp, timestamp + 2)
     end
 
     it 'include project id' do
-      expect(uniq_id).to include("-#{context_id}-")
+      expect(uniq_id).to include(sprintf('-%02x-', context_id))
     end
   end
 
   describe '#generate' do
-    context 'with valid project id' do
-      let(:context_id) { 999 }
+    context 'with valid context id' do
+      let(:context_id) { rand(255) }
 
       it 'generates random id every time' do
         expect(uniq_id).to_not eq(uniq_id)
       end
     end
 
-    context 'with invalid project id' do
+    context 'with empty context id' do
       let(:context_id) { nil }
+
+      it 'raises exception' do
+        expect { uniq_id }.to raise_error(IdGenerator::Error,
+                                          'Invalid project id')
+      end
+    end
+
+    context 'with invalid context id' do
+      let(:context_id) { 'context' }
+
+      it 'raises exception' do
+        expect { uniq_id }.to raise_error(IdGenerator::Error,
+                                          'Invalid project id')
+      end
+    end
+
+    context 'with invalid too big context id' do
+      let(:context_id) { 267 }
+
+      it 'raises exception' do
+        expect { uniq_id }.to raise_error(IdGenerator::Error,
+                                          'Invalid project id')
+      end
+    end
+
+    context 'with negative context id' do
+      let(:context_id) { -5 }
 
       it 'raises exception' do
         expect { uniq_id }.to raise_error(IdGenerator::Error,
