202c80dd1d042601b2775329b8bd3269a9480585
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2018-10-22 14:20:44 +0300

    Add and reformat tests

--- spec/id_generator/generator_spec.rb
+++ spec/id_generator/generator_spec.rb
@@ -1,18 +1,30 @@
 RSpec.describe IdGenerator do
   let(:generator) { described_class.new(context_id) }
 
-  describe '#generate' do
-    subject(:uniq_id) { generator.generate }
+  describe 'id format' do
+    let(:context_id) { 999 }
 
+    it 'has correct size' do
+      expect(uniq_id.size).to eq(34)
+    end
+
+    it 'includes timestamp' do
+      timestamp = Time.now.to_i - Time.new(2014).to_i
+      id_timestamp = uniq_id.split('-').first.to_i(16)
+      expect(id_timestamp).to be_between(timestamp, timestamp+2)
+    end
+
+    it 'include project id' do
+      expect(uniq_id).to include("-#{context_id}-")
+    end
+  end
+
+  describe '#generate' do
     context 'with valid project id' do
       let(:context_id) { 999 }
 
-      it 'generates uniq id with correct size' do
-        expect(uniq_id.size).to eq(34)
-      end
-
-      it 'generates uniq id that include projec id' do
-        expect(uniq_id).to include("-#{context_id}-")
+      it 'generates random id every time' do
+        expect(uniq_id).to_not eq(uniq_id)
       end
     end
 
@@ -24,5 +36,11 @@ RSpec.describe IdGenerator do
                                           'Invalid project id')
       end
     end
+  end
+
+  protected
+
+  def uniq_id
+    generator.generate
   end
 end
